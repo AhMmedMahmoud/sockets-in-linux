cdd2aa27b200c27a0b1fca182f114e20c6164660
Ahmed Mahmoud <104006521+AhMmedMahmoud@users.noreply.github.com>
2023-10-30 15:58:06 +0200

    Add files via upload

--- src/tcp_client.cpp
+++ src/tcp_client.cpp
@@ -1,6 +1,9 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include "../include/tcp_client.h"
+// for print 
+#include <iostream>
+
 
 namespace AsyncBsdSocketLib
 {
@@ -11,16 +14,38 @@ namespace AsyncBsdSocketLib
     bool TcpClient::TrySetup() noexcept
     {
         FileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+        /*
+            SOCK_STREAM
+               Provides sequenced, reliable, two-way, connection-based
+               byte streams (TCP).
+
+            SOCK_DGRAM
+               Supports datagrams (connectionless, unreliable messages of
+               a fixed maximum length)  (UDP).
+        */
+        /*
+            AF_INET      IPv4 Internet protocols
+            AF_INET6     IPv6 Internet protocols
+        */
 
         bool _result = (FileDescriptor >= 0);
-
         if (_result)
         {
             mAddress.sin_addr.s_addr = inet_addr(IpAddress.c_str());
-            mAddress.sin_family = AF_INET;
+            mAddress.sin_family = AF_INET;      
             mAddress.sin_port = htons(Port);
-        }
 
+            std::cout << "-----------------Socket info--------------\n";
+            if(mAddress.sin_family == AF_INET) 
+                std::cout << "IPv4 Address" << std::endl;
+            else if (mAddress.sin_family == AF_INET6) 
+                std::cout << "IPv6 Address" << std::endl;
+            else 
+                std::cout << "Unknown Address Type" << std::endl;
+            std::cout << "IP Address: " << inet_ntoa(mAddress.sin_addr) << std::endl;
+            std::cout << "Port: " << ntohs(mAddress.sin_port) << std::endl;
+            std::cout << "------------------------------------------\n";
+        }
         return _result;
     }
 
